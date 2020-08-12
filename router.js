b9a4cb2a96cdd9307137a4c32ad3153d20440f45
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-08-12 17:53:41 +0100

    created router

---
+++ router.js
@@ -0,0 +1,24 @@
+//we need handlers for home, public, missing, submit
+//also for the model so the server can talk to the database?
+
+const homeHandler = require("./handlers/home");
+const publicHandler = require("./handlers/public");
+const missingHandler = require("./handlers/missing");
+const submitHandler = require("./handlers/submit");
+
+function router(request, response) {
+    const url = request.url;
+    const method = request.method;
+
+    if (url === "/") {
+        homeHandler(request, response);
+    } else if (url.includes("public")) {
+        publicHandler(request, response);
+    } else if (url.includes("submit" && method == "POST")) 
+        submitHandler(request, response);
+    } else {
+        missingHandler(request, response);
+    }
+}
+
+module.exports = router;
