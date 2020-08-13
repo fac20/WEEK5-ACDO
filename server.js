5558ad49a6ac035666decfe021b26c6c3a92b241
mhtien <59407030+mhtien@users.noreply.github.com>
2020-08-13 17:19:55 +0100

    change server to include hostname

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require("http");
 const PORT = process.env.port || 3000;
+const hostname = process.env.HOSTNAME || "0.0.0.0" || "localhost";
 const router = require("./router");
+
 
 const server = http.createServer(router);
 
 
-server.listen(PORT, () => console.log(`Woof! Listening on http://localhost:${PORT}`));
+server.listen(PORT, hostname, () => console.log(`Woof! Listening on http://${hostname}:${PORT}`));
 
 // catch server error here 
