fd365604ff263c8410ad95abf4bd0c913a0491eb
mhtien <59407030+mhtien@users.noreply.github.com>
2020-08-13 17:29:45 +0100

    changing port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME || "0.0.0.0" || "localhost";
 const router = require("./router");
 
