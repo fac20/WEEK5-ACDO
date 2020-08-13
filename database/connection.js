37e9664b64716f7db5a26d43eab5421a40f764a7
mhtien <59407030+mhtien@users.noreply.github.com>
2020-08-13 14:17:32 +0100

    update database connection
    Co-authored-by: Aishah <aishah92@gmail.com>
    
    Relates #1 and #2

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,11 @@
-require("./dotenv").config();
+const dotenv = require("dotenv");
 const pg = require("pg");
+dotenv.config();
+console.log(process.env.DATABASE_URL);
+const connectStr = process.env.DATABASE_URL;
 
-const connectStr = process.env.CONNECTION;
+const db = new pg.Pool({ connectStr });
 
-const db = new pg.Pool({ connectStr });const db = new pg.Pool({ connectStr });
+db.query("SELECT * FROM users").then((result)=> console.log(result));
 
 module.exports = db;
