4d9fb4e098591c2dbbf924c5bbce055679fb063f
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-14 11:19:55 +0100

    Fixes comments and try catch in models Fixes #32 Fixes #29 Co-authored-by: My Hoa Tien <myhoatien@gmail.com> Co-authored-by: Aishah <aishah92@gmail.com> Co-authored-by: Jessica <j.r.deacon@hotmail.co.uk>

--- public/script.js
+++ public/script.js
@@ -1,8 +1,4 @@
-// const form = document.querySelector(".forminput__form")
 
-// const putDate = form => {
-//     form.date.value = newDate().toString();
-// }
 
 const submitBtn = document.querySelector("input[type=submit]");
 
