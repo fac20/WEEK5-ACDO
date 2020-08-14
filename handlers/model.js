1fd1c6475f7d35b114e70e300bde0ce1a24923e9
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-14 11:49:19 +0100

    Fixing unique id bug Co-authored-by: My Hoa Tien <myhoatien@gmail.com> Co-authored-by: Aishah <aishah92@gmail.com> Co-authored-by: Jessica <j.r.deacon@hotmail.co.uk>

--- handlers/model.js
+++ handlers/model.js
@@ -9,7 +9,8 @@ function getPostsData(){
  const add = async function(name, msgtitle, message) {
      try {
         let nameInsertion = await db.query("INSERT INTO users(username) VALUES($1)", [name])//if duplicate values then unique constraint will throw error. Need to have an error block to handle this.
-        let postInsertion = await db.query("INSERT INTO posts(post_title, text_content, user_id) VALUES($1, $2, (SELECT id FROM users WHERE username = $3))", [msgtitle, message, name])
+        let postInsertion = await db.query("INSERT INTO posts(post_title, text_content, user_id) VALUES($1, $2, (SELECT id FROM users WHERE username = $3 LIMIT 1)))", [msgtitle, message, name])
+        
 
         return nameInsertion, postInsertion;
      }
